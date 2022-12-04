b3f8783d52f60be5c587b6f0288acdadfd715998
Dunridge <Polinka7max@gmail.com>
2022-12-04 17:09:11 +0200

    develop. update music volume

--- scripts/script.js
+++ scripts/script.js
@@ -5,8 +5,10 @@ $(document).ready(
   (function () {
     var iframeElement = document.querySelector("iframe");
     var widget = SC.Widget(iframeElement);
-
+    // api methods -- https://developers.soundcloud.com/docs/api/html5-widget#methods
+    
     guitarContainer.addEventListener("click", () => {
+      widget.setVolume(40);
       widget.toggle();
     });
   })()
